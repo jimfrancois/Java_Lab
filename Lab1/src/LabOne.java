add0f9cf0d0fc886dc3a8d2889cdd9e8e2ccf9d0
jimitryfrancois98@gmail.com <jimitryfrancois98@gmail.com>
2022-02-10 21:17:30 -0500

    The first lab

---
+++ Lab1/src/LabOne.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class LabOne {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your name: ");
+		String firstName = input.nextLine();
+		
+		System.out.println("Hello "+ firstName);
+		
+	}
+
+}
